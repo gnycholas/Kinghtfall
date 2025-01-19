090ab23a5163a88f9cc7e5d90bf5fc7a9926dd73
SoHaiNp <l.guinycholas@gmail.com>
2025-01-19 14:34:59 -0300

    coleta da chave permitindo abrir a porta

---
+++ Assets/Scripts/AbrirPorta.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirPorta : MonoBehaviour
+{
+    [SerializeField] private Animator animatorPorta;
+    [SerializeField] private string itemNecessario = "key";
+
+    private bool jogadorPerto = false;
+    private MovimentoPersonagem playerMovement;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jogadorPerto = true;
+            playerMovement = other.GetComponent<MovimentoPersonagem>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jogadorPerto = false;
+            playerMovement = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (jogadorPerto && Input.GetKeyDown(KeyCode.E))
+        {
+            if (playerMovement.inventario.Contains(itemNecessario))
+            {
+                animatorPorta.Play("animationDoor", 0, 0.0f);
+                Debug.Log("Porta aberta!");
+            }
+            else
+            {
+                Debug.Log("Você não tem a chave para abrir esta porta!");
+            }
+        }
+    }
+}
+
+
