a25e20689ea125e03076b02dc2221e3e86f1f186
Valdeir Antonio do Nascimento <valdeir.ans@gmail.com>
2025-04-22 11:50:30 -0300

    update

---
+++ Assets/Scripts/Tutorial/DaggerTutorialController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class DaggerTutorialController : TutorialController
+{
+    [SerializeField] private float _time;
+    [SerializeField] private GameObject _moveWarmPrefab;
+     
+    public async override void SetupTutorial()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(_time));
+        await UniTask.Delay(TimeSpan.FromSeconds(2));
+        var warm = Instantiate(_moveWarmPrefab);
+        TriggerTutorial(async () => 
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(4));
+            return true;
+        }, async () =>
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(2));
+            Destroy(warm.gameObject);
+        }).Forget();
+    }
+}
---
+++ Assets/Scripts/Tutorial/PottionTutorialController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class PottionTutorialController : TutorialController
+{
+    [SerializeField] private float _time;
+    [SerializeField] private GameObject _moveWarmPrefab;
+
+    public async override void SetupTutorial()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(_time));
+        var warm = Instantiate(_moveWarmPrefab);
+        TriggerTutorial(async () =>
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(4));
+            return true;
+        }, async () =>
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(2));
+            Destroy(warm.gameObject);
+        }).Forget();
+    }
+}
